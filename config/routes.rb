75871c621c9c433668bec5cfc3b4bea564c811d9
ranasulin <ran@bitsofgold.co.il>
2018-08-08 18:29:22 +0300

    add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/contact'
-
-  get 'welcome/index'
-
-  get 'welcome/about'
+  resources :posts
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 end
