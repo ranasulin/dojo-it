d91385ed8f3b1e1dc3f292bbcb754f3f4921d2e5
ranasulin <ran@bitsofgold.co.il>
2018-08-13 10:23:11 +0300

    add roles

--- Gemfile
+++ Gemfile
@@ -59,3 +59,4 @@ end
 
 gem 'devise'
 gem 'figaro'
+gem 'pundit'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180812132205) do
+ActiveRecord::Schema.define(version: 20180812151744) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20180812132205) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
