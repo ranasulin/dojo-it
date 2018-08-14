2061404189ddffe7b6254578794da206f1bd01fe
ranasulin <ran@bitsofgold.co.il>
2018-08-14 10:27:42 +0300

    add redcarpet

--- Gemfile
+++ Gemfile
@@ -60,3 +60,4 @@ end
 gem 'devise'
 gem 'figaro'
 gem 'pundit'
+gem 'redcarpet'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,7 @@
 class PostsController < ApplicationController
-
   def show
-    @post = Post.find(params[:id])
     @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -17,19 +16,6 @@ class PostsController < ApplicationController
     authorize @post
   end
 
-  def update
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:id])
-    authorize @post
-    if @post.update_attributes(post_params)
-      flash[:notice] = "Post was updated."
-      redirect_to @post
-    else
-      flash[:error] = "There was an error saving the post. Please try again."
-      render :edit
-    end
-  end
-
   def create
     @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(post_params)
@@ -37,10 +23,23 @@ class PostsController < ApplicationController
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
+    end
+  end
+  
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Post was updated."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
     end
   end
 
