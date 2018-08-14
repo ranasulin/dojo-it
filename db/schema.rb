a4afa307cc7114570f23e4ea2eebe1fd46964e81
ranasulin <ran@bitsofgold.co.il>
2018-08-14 15:05:03 +0300

    upload photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180813073010) do
+ActiveRecord::Schema.define(version: 20180814074210) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20180813073010) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
