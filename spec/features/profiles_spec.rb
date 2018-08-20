b8e4b3926ee2046c58f5d4523803b06fb99e25a3
ranasulin <ran@bitsofgold.co.il>
2018-08-20 14:28:13 +0300

    add another test for signed in user

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -22,4 +22,19 @@ describe "Visiting profiles" do
     end
 
   end
+
+  describe "signed in" do
+
+    before {login_as(@user, :scope => :user)}
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
+
+  end
 end
